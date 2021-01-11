e466814f3983d3411c9e73e11c01a336ff20b8e0
chenhaodong <15862713862@163.com>
2021-01-11 22:00:59 +0800

    修改等天黑

--- chdJavaWeb/src/com/chd/git/chd.java
+++ chdJavaWeb/src/com/chd/git/chd.java
@@ -3,5 +3,10 @@ package com.chd.git;
 public class chd {
     public static void main(String[] args) {
         System.out.println("等天黑");
+        //只能增
+        System.out.println("16515616651651651");
+
+        }
     }
+
 }
